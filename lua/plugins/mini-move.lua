c0b79038069384c1cb8743ef7309c1a2febbd47e
song-giung <rldnddl87@gmail.com>
2025-04-10 16:12:36 +0900

    mini move fix

---
+++ lua/plugins/mini-move.lua
@@ -0,0 +1,17 @@
+return {
+  "echasnovski/mini.move",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      left = "<leader>h",
+      right = "<leader>l",
+      down = "<leader>j",
+      up = "<leader>k",
+
+      line_left = "<leader>h",
+      line_right = "<leader>l",
+      line_down = "<leader>j",
+      line_up = "<leader>k",
+    },
+  },
+}
