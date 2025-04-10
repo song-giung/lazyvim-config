856943b4f259a165be1349752968fa4224cde499
song-giung <rldnddl87@gmail.com>
2025-04-10 09:20:33 +0900

    add keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
+if vim.g.vscode then
+    local keymap = vim.keymap.set
+    local opts = { noremap = true, silent = true }
 
--- remap leader key
-keymap("n", "<Space>", "", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+    -- remap leader key
+    keymap("n", "<Space>", "", opts)
+    vim.g.mapleader = " "
+    vim.g.maplocalleader =nn "
+
+    -- rename symbol
+    vim.keymap.set("n", "<leader>rn", function()
+        vim.fn.VSCodeNotify("editor.action.rename")
+    end, { desc = "Rename Symbol" })
+end
