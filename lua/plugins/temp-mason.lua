3c37f9ef3756469640e849a1535e097202c5879b
song-giung <rldnddl87@gmail.com>
2025-05-13 08:42:56 +0900

    remove temp mason fix version

--- lua/plugins/temp-mason.lua
+++
@@ -1,6 +0,0 @@
--- temp for mason, mason-lspconfig v2 update
--- https://github.com/LazyVim/LazyVim/issues/6039
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
