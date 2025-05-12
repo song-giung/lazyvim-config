a0126be073e546d24ae464b113f8028f087243d2
song-giung <rldnddl87@gmail.com>
2025-05-12 14:43:53 +0900

    add nvim-lint, bugfix for temp-mason, update lock

---
+++ lua/plugins/temp-mason.lua
@@ -0,0 +1,6 @@
+-- temp for mason, mason-lspconfig v2 update
+-- https://github.com/LazyVim/LazyVim/issues/6039
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
